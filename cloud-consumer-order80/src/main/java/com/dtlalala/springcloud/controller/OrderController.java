d7ee4dca064b3ba7e5a8cdc3eaf68977efd5725b
“dtlalala <dtlalala@qq.com>
2021-03-16 23:13:47 +0800

    payment服务集群配置，主机服务名称修改

--- cloud-consumer-order80/src/main/java/com/dtlalala/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/dtlalala/springcloud/controller/OrderController.java
@@ -13,7 +13,8 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderController {
-    public static final String PAYMENT_URL = "http://localhost:8001";
+//    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -25,6 +26,6 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(PAYMENT_URL+"payment/get/"+id, CommonResult.class);
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
     }
 }
---
+++ cloud-provider-payment8002/src/main/java/com/dtlalala/springcloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.dtlalala.springcloud.service;
+
+import com.dtlalala.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
